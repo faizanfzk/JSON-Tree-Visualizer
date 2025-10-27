65a21f6fc889cb170c97a9ff47a1957ea31a8ee5
Faizan Khan <97458887+faizanfzk@users.noreply.github.com>
2025-10-27 23:59:09 +0530

    feat : adds textarea for json input and adds styles

--- Json-visualizer/src/App.tsx
+++ Json-visualizer/src/App.tsx
@@ -1,11 +1,54 @@
 
+import { useState } from 'react'
 import './App.css'
+import { placeholder } from './static-data/static-data';
+
 
 function App() {
 
+  const [isValidJson, setIsValidJson] = useState(true);
+  const [jsonText, setJsonText] = useState('');
+
+
+  const handleChange = (e: any) => {
+    const value = e.target.value;
+    setJsonText(value)
+    setIsValidJson(true)
+
+  }
+
+  const validateJson = () => {
+    try {
+      JSON.parse(jsonText)
+      setIsValidJson(true)
+    } catch {
+      setIsValidJson(false)
+    }
+  }
+
   return (
-    <div>
-      <h1>JSON Tree Visualizer</h1>
+    <div className='container'>
+      <div className='json-input-wrapper'>
+        <h1>JSON Tree Visualizer</h1>
+        {
+          (!isValidJson) && <p style={{ color: 'red', fontWeight: 500 }}>Invalid JSON</p>
+        }
+        <div>
+          <p className='sub-heading'>Paste or type your JSON data </p>
+          <textarea value={jsonText}
+            className='textarea-box'
+            rows={25}
+            cols={60}
+            placeholder={placeholder}
+            style={{ border: !isValidJson ? '1px solid red' : '' }}
+            onChange={handleChange}
+          />
+        </div>
+        <button className='generate-tree-btn' disabled={!jsonText} onClick={validateJson}>
+          Generate Tree
+        </button>
+
+      </div>
     </div>
   )
 }
